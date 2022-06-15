c95da71607e742e86180721a9bb039857075a419
Mazdak Pakaghideh <Mazdak.software@gmail.com>
2022-06-15 18:32:09 +0430

    Inited Tailwind.css

---
+++ mazdakdev/nuxt.config.ts
@@ -0,0 +1,21 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    target: 'static',
+    
+    build: {
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {},
+                }
+            },
+        }
+    },
+
+    css: [
+        '~/assets/css/main.css',
+    ],
+})
---
+++ mazdakdev/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex justify-center items-center text-white ">
+        
+    </div>
+</template>
