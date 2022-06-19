061127ce6a9fbe44e7b83002c80b0a6d2eb61c2f
Mazdak Pakaghideh <Mazdak.software@gmail.com>
2022-06-19 17:08:34 +0430

    Completed Home page

--- mazdakdev/pages/index.vue
+++ mazdakdev/pages/index.vue
@@ -4,11 +4,12 @@
 
         <div class="md:hidden">
             <div id="burgerBtn" class="mt-6 ml-2" @click="burgerBtn" ></div>
-            <ul id="nav" :class="nav_status">
-               <li></li>
-                <li></li> 
-                <li></li>
-                <li></li>
+            <ul id="nav" :class="nav_status" class="text-gray-400 text-xl ">
+                <a href=""><li class="hover:text-gray-200">Home</li></a>
+                <a href=""><li class="hover:text-gray-200">Blog</li></a>
+                <a href=""><li class="hover:text-gray-200">Snippets</li></a>
+                <a href=""><li class="hover:text-gray-200">Projects</li></a>
+                <a href=""><li class="hover:text-gray-200">CV</li></a>
             </ul>
         </div>
 
@@ -73,21 +74,24 @@
                 Featured Posts
             </h3>
 
-            <div class="flex gap-6 flex-col md:flex-row mt-6  ">
-                <div class="post-1 md:w-[208px] md:h-[210px] h-40 shadow-sm justify-center items-center md:justify-start md:items-start flex p-5 ">
-                    <p class="text-white text-xl text-center  md:text-start ">The Fact that Soraya is no one but Mazdak</p>
+            <div class="flex gap-6 flex-col md:flex-row mt-6 ">
+                <div class="post-1 md:w-[208px] md:h-[210px] h-40 shadow-sm justify-center items-center  flex p-5 hover:scale-[1.01] ">
+                    <p class="text-white text-xl text-center  md:text-start ">5 Essential things
+                        You must do in 2022 </p>
                 </div>
 
-                <div class="post-2  shadow-sm md:w-[208px] md:h-[210px] h-40 justify-center items-center md:justify-start md:items-start flex p-5">
-                    <p class="text-white text-xl  text-center md:text-start  ">The Fact that Soraya is no one but Mazdak</p>
+                <div class="post-2  shadow-sm md:w-[208px] md:h-[210px] h-40 justify-center items-center  flex p-5 hover:scale-[1.01]">
+                    <p class="text-white text-xl  text-center md:text-start  ">Relation between philosophy and 
+                         programming 
+                    </p>
                 </div>
 
-                <div class="post-3 shadow-sm md:w-[208px] md:h-[210px] h-40 justify-center items-center md:justify-start md:items-start  flex p-5">
-                    <p class="text-white text-xl text-center md:text-start ">The Fact that Soraya is no one but Mazdak</p>
+                <div class="post-3 shadow-sm md:w-[208px] md:h-[210px] h-40 justify-center items-center   flex p-5 hover:scale-[1.01]">
+                    <p class="text-white text-xl text-center md:text-start ">You must avoid this 5 things in 2022 !</p>
                 </div>
             </div>
 
-            <a class="flex mt-8 items-start-start justify-start  text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6">
+            <a class="flex mt-8 items-start-start justify-start  text-gray-400 leading-7 rounded-lg hover:text-gray-200 transition-all h-6">
                 Read all posts
 
                 <svg
@@ -133,12 +137,12 @@
         </h3>
 
         <div class="flex mt-6 flex-row items-center justify-center md:ml-14 md:space-x-52 space-x-10 ">
-            <button class="cv md:ml-30   h-[56px]  flex flex-col md:flex-row  items-center  justify-center p-7 ">
+            <button class="cv md:ml-30   h-[56px]  flex flex-col md:flex-row  items-center hover:scale-[1.05] justify-center p-7 ">
                 <p class="text-gray-100">CV - EN</p>
                 <svg  class="fill-gray-100 w-[16px] h-[15px] md:ml-5 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M480 352h-133.5l-45.25 45.25C289.2 409.3 273.1 416 256 416s-33.16-6.656-45.25-18.75L165.5 352H32c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h448c17.67 0 32-14.33 32-32v-96C512 366.3 497.7 352 480 352zM432 456c-13.2 0-24-10.8-24-24c0-13.2 10.8-24 24-24s24 10.8 24 24C456 445.2 445.2 456 432 456zM233.4 374.6C239.6 380.9 247.8 384 256 384s16.38-3.125 22.62-9.375l128-128c12.49-12.5 12.49-32.75 0-45.25c-12.5-12.5-32.76-12.5-45.25 0L288 274.8V32c0-17.67-14.33-32-32-32C238.3 0 224 14.33 224 32v242.8L150.6 201.4c-12.49-12.5-32.75-12.5-45.25 0c-12.49 12.5-12.49 32.75 0 45.25L233.4 374.6z"/></svg>
             </button>
             
-            <button class="cv md:ml-30  h-[56px]  flex flex-col md:flex-row  items-center justify-center p-7 ">
+            <button class="cv md:ml-30  h-[56px]  flex flex-col md:flex-row hover:scale-[1.05] items-center justify-center p-7 ">
                 <p class="text-gray-100">CV - EN</p>
                 <svg  class="fill-gray-100 w-[16px] h-[15px] md:ml-5 " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M480 352h-133.5l-45.25 45.25C289.2 409.3 273.1 416 256 416s-33.16-6.656-45.25-18.75L165.5 352H32c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h448c17.67 0 32-14.33 32-32v-96C512 366.3 497.7 352 480 352zM432 456c-13.2 0-24-10.8-24-24c0-13.2 10.8-24 24-24s24 10.8 24 24C456 445.2 445.2 456 432 456zM233.4 374.6C239.6 380.9 247.8 384 256 384s16.38-3.125 22.62-9.375l128-128c12.49-12.5 12.49-32.75 0-45.25c-12.5-12.5-32.76-12.5-45.25 0L288 274.8V32c0-17.67-14.33-32-32-32C238.3 0 224 14.33 224 32v242.8L150.6 201.4c-12.49-12.5-32.75-12.5-45.25 0c-12.49 12.5-12.49 32.75 0 45.25L233.4 374.6z"/></svg>
             </button>
@@ -149,7 +153,7 @@
 
     </div>
     </div>
-     <footer class="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
+    <footer class="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
       <hr class="w-full border-1 border-gray-800 mb-8" />
 
       <div class="flex m-auto mt-8 space-x-6 text-gray-200 lg:mt-0   ">       
@@ -306,7 +310,7 @@
     height:30px;
     width:0;
     margin:30px 0;
-    background:#eae8e5;
+
     @include transition(all 0.6s ease-in);
     & + li {
       margin-left:-40px;
