117efa82f681886e284a140209b9c0ab3dde0aed
Mazdak Pakaghideh <Mazdak.software@gmail.com>
2022-06-28 15:36:57 +0430

    Added social links & blog api

---
+++ mazdakdev/components/Nav.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>          
+        <nav class="py-7 rounded ">
+            <div class="md:container md:flex md:flex-col md:justify-center md:items-start md:mx-auto">
+                <div class="hidden md:flex md:w-auto md:order-1"  id="mobile-menu-4">
+                    <ul class="flex flex-col mt-4 md:flex-row md:space-x-4 md:mt-0">
+                        <li>
+                            <a href="#" class="block py-2 pr-4  text-gray-200  rounded p-0 " aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pr-4  text-gray-400 hover:text-gray-200 rounded p-0">Blog</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pr-4  text-gray-400 hover:text-gray-200  rounded p-0">Snippets</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pr-4  text-gray-400  hover:text-gray-200 rounded p-0">Projects</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pr-4  text-gray-400 hover:text-gray-200 rounded p-0">CV</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
